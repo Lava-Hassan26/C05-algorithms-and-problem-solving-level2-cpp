9403cfda8ecebdc53ef6655f9bf74533f0962a26
Lava Hassan <lavahasan827@gmail.com>
2026-09-05 00:17:35 +0300

    Add solution for P08

---
+++ P08-DigitFrequency/P08-DigitFrequency.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadPositiveNumber(string message)
+{
+    int number = 0;
+    do
+    {
+        cout << message;
+        cin >> number;
+    } while (number <= 0);
+
+    return number;
+}
+
+short CountDigitFrequency(short digit, int number)
+{
+    int remainder = 0;
+    short frequency = 0;
+
+    do
+    {
+        remainder = number % 10;
+        number = number / 10;
+
+        if (remainder == digit)
+            frequency++;
+
+    } while (number > 0);
+
+    return frequency;
+}
+
+int main()
+{
+    int number = ReadPositiveNumber("Please enter the main number: ");
+    short digit = ReadPositiveNumber("Please enter one digit to check: ");
+
+    cout << "\nDigit " << digit << " Frequency is "
+        << CountDigitFrequency(digit, number) << " Time(s).\n";
+
+    return 0;
+}
