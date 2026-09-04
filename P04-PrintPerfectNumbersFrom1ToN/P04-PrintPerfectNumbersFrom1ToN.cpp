4551f588d30c7c110c40360e4458a3ec21274074
Lava Hassan <lavahasan827@gmail.com>
2026-09-05 00:13:59 +0300

    Add solution for P04

---
+++ P04-PrintPerfectNumbersFrom1ToN/P04-PrintPerfectNumbersFrom1ToN.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadPositiveNumber(string message)
+{
+    int number = 0;
+    do
+    {
+        cout << message;
+        cin >> number;
+    } while (number <= 0);
+
+    return number;
+}
+
+bool IsPerfectNumber(int number)
+{
+    int sum = 0;
+
+    for (int i = 1; i <= number / 2; i++)
+    {
+        if (number % i == 0)
+            sum += i;
+    }
+
+    return number == sum;
+}
+
+void PrintPerfectNumbersFrom1ToN(int N)
+{
+    cout << "\nPerfect Numbers from 1 to " << N << " are:\n";
+
+    for (int i = 1; i <= N; i++)
+    {
+        if (IsPerfectNumber(i))
+            cout << i << endl;
+    }
+}
+
+int main()
+{
+    PrintPerfectNumbersFrom1ToN(ReadPositiveNumber("Enter a positive number: "));
+
+    return 0;
+}
