a98890eb2560c907ba314637fdb2bb18f2a549b6
Lava Hassan <lavahasan827@gmail.com>
2026-09-05 00:14:32 +0300

    Add solution for P05

---
+++ P05-PrintDigitsInReversedOrder/P05-PrintDigitsInReversedOrder.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadPositiveNumber(string message)
+{
+    int number = 0;
+    do
+    {
+        cout << message;
+        cin >> number;
+    } while (number <= 0);
+
+    return number;
+}
+
+void PrintDigitsInReversedOrder(int number)
+{
+    int remainder = 0;
+
+    do
+    {
+        remainder = number % 10;
+        number = number / 10;
+        cout << remainder << endl;
+    } while (number > 0);
+}
+
+int main()
+{
+    PrintDigitsInReversedOrder(ReadPositiveNumber("Enter a positive number: "));
+
+    return 0;
+}
