c184a6ff509564628f1798f3c644f00709e6e11c
Lava Hassan <lavahasan827@gmail.com>
2026-09-05 00:15:44 +0300

    Add solution for P06

---
+++ P06-SumOfDigits/P06-SumOfDigits.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadPositiveNumber(string message)
+{
+    int number = 0;
+    do
+    {
+        cout << message;
+        cin >> number;
+    } while (number <= 0);
+
+    return number;
+}
+
+int SumOfDigits(int number)
+{
+    int sum = 0;
+    int remainder = 0;
+
+    do
+    {
+        remainder = number % 10;
+        number = number / 10;
+        sum += remainder;
+    } while (number > 0);
+
+    return sum;
+}
+
+void PrintSumOfDigits(int number)
+{
+    cout << "\nSum of Digits = " << SumOfDigits(number) << endl;
+}
+
+int main()
+{
+    PrintSumOfDigits(ReadPositiveNumber("Enter a positive number: "));
+
+    return 0;
+}
