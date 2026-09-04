d79a3dc81c83917550040238a4a57f341bc44232
Lava Hassan <lavahasan827@gmail.com>
2026-09-05 00:18:45 +0300

    Add solution for P10

---
+++ P10-PrintDigitsInOrder/P10-PrintDigitsInOrder.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadPositiveNumber(string message)
+{
+    int number = 0;
+    do
+    {
+        cout << message;
+        cin >> number;
+    } while (number <= 0);
+
+    return number;
+}
+
+int ReverseNumber(int number)
+{
+    int remainder = 0;
+    int reversedNumber = 0;
+
+    do
+    {
+        remainder = number % 10;
+        number = number / 10;
+        reversedNumber = reversedNumber * 10 + remainder;
+    } while (number > 0);
+
+    return reversedNumber;
+}
+
+void PrintDigits(int number)
+{
+    int remainder = 0;
+
+    do
+    {
+        remainder = number % 10;
+        number = number / 10;
+        cout << remainder << endl;
+    } while (number > 0);
+}
+
+int main()
+{
+    PrintDigits(ReverseNumber(ReadPositiveNumber("Enter a positive number: ")));
+
+    return 0;
+}
