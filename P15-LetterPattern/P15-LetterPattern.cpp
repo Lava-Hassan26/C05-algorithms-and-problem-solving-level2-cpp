8f63296367b440d43066c36c2ae23b0cdef02bb7
Lava Hassan <lavahasan827@gmail.com>
2026-09-09 23:02:25 +0300

    Add solution for P15

---
+++ P15-LetterPattern/P15-LetterPattern.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadPositiveNumber(string message)
+{
+    int number = 0;
+    do
+    {
+        cout << message;
+        cin >> number;
+    } while (number <= 0);
+
+    return number;
+}
+
+void PrintLetterPattern(int number)
+{
+    cout << endl;
+
+    for (short i = 1; i <= number; i++)
+    {
+        for (short j = 1; j <= i; j++)
+        {
+            cout << char(i + 64);
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    PrintLetterPattern(ReadPositiveNumber("Please enter a positive number: "));
+
+    return 0;
+}
