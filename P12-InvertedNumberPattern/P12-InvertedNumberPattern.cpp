4eabcc8b03f5d671d1537acb856d273dd80427fb
Lava Hassan <lavahasan827@gmail.com>
2026-09-09 23:00:52 +0300

    Add solution for P12

---
+++ P12-InvertedNumberPattern/P12-InvertedNumberPattern.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadPositiveNumber(string message)
+{
+    int number = 0;
+    do
+    {
+        cout << message;
+        cin >> number;
+    } while (number <= 0);
+
+    return number;
+}
+
+void PrintInvertedNumberPattern(int number)
+{
+    cout << endl;
+
+    for (short i = number; i >= 1; i--)
+    {
+        for (short j = 1; j <= i; j++)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    PrintInvertedNumberPattern(ReadPositiveNumber("Please enter a positive number: "));
+
+    return 0;
+}
