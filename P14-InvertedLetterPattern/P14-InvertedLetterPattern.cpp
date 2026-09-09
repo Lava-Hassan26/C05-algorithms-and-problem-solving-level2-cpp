b1ab8d346a4f3f39b42a8bfa7312708eeea67f76
Lava Hassan <lavahasan827@gmail.com>
2026-09-09 23:02:05 +0300

    Add solution for P14

---
+++ P14-InvertedLetterPattern/P14-InvertedLetterPattern.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadPositiveNumber(string message)
+{
+    int number = 0;
+    do
+    {
+        cout << message;
+        cin >> number;
+    } while (number <= 0);
+
+    return number;
+}
+
+void PrintInvertedLetterPattern(int number)
+{
+    cout << endl;
+
+    for (short i = number; i > 0; i--)
+    {
+        for (short j = 1; j <= i; j++)
+        {
+            cout << char(i + 64);
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    PrintInvertedLetterPattern(ReadPositiveNumber("Please enter a positive number: "));
+
+    return 0;
+}
