467b1b758f2b90aeb828387a10b7fd4c3ae64aa2
Lava Hassan <lavahasan827@gmail.com>
2026-09-09 23:01:46 +0300

    Add solution for P13

---
+++ P13-NumberPattern/P13-NumberPattern.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadPositiveNumber(string message)
+{
+    int number = 0;
+    do
+    {
+        cout << message;
+        cin >> number;
+    } while (number <= 0);
+
+    return number;
+}
+
+void PrintNumberPattern(int number)
+{
+    cout << endl;
+
+    for (short i = 1; i <= number; i++)
+    {
+        for (short j = 1; j <= i; j++)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    PrintNumberPattern(ReadPositiveNumber("Please enter a positive number: "));
+
+    return 0;
+}
