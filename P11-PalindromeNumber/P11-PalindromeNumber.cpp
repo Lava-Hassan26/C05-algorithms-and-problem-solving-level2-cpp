063a31635fdd57f5d0c1f6e2a11ee50fe3c68640
Lava Hassan <lavahasan827@gmail.com>
2026-09-09 22:59:53 +0300

    Add solution for P11

---
+++ P11-PalindromeNumber/P11-PalindromeNumber.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadPositiveNumber(string message)
+{
+    int number = 0;
+    do
+    {
+        cout << message;
+        cin >> number;
+    } while (number <= 0);
+
+    return number;
+}
+
+int ReverseNumber(int number)
+{
+    int remainder = 0;
+    int reversedNumber = 0;
+
+    do
+    {
+        remainder = number % 10;
+        number = number / 10;
+        reversedNumber = reversedNumber * 10 + remainder;
+    } while (number > 0);
+
+    return reversedNumber;
+}
+
+bool IsPalindromeNumber(int number)
+{
+    return number == ReverseNumber(number);
+}
+
+void PrintResult(int number)
+{
+    cout << endl;
+    if (IsPalindromeNumber(number))
+        cout << "Yes, it is a Palindrome number.\n";
+    else
+        cout << "No, it is NOT a Palindrome number.\n";
+}
+
+int main()
+{
+    PrintResult(ReadPositiveNumber("Please enter a positive number: "));
+
+    return 0;
+}
