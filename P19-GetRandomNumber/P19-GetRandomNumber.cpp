883e88bded8c1145f3077586c0cae43bb53cacc3
Lava Hassan <lavahasan827@gmail.com>
2026-09-14 23:25:32 +0300

    Add solution for P19

---
+++ P19-GetRandomNumber/P19-GetRandomNumber.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int GetRandomNumber(int from, int to)
+{
+    int randNumber = rand() % (to - from + 1) + from;
+    return randNumber;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    cout << "Random number1: " << GetRandomNumber(1, 100) << endl;
+    cout << "Random number2: " << GetRandomNumber(1, 100) << endl;
+    cout << "Random number3: " << GetRandomNumber(1, 100) << endl;
+
+    return 0;
+}
