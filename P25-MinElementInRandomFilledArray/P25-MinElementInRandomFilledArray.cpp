3ef3deceeaff0d554846a3a125c60c0f519a1159
Lava Hassan <lavahasan827@gmail.com>
2026-09-15 23:00:27 +0300

    Add solution for P25

---
+++ P25-MinElementInRandomFilledArray/P25-MinElementInRandomFilledArray.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int GetRandomNumber(int from, int to)
+{
+    int randNumber = rand() % (to - from + 1) + from;
+    return randNumber;
+}
+
+int ReadPositiveNumber(string message)
+{
+    int number = 0;
+    do
+    {
+        cout << message;
+        cin >> number;
+    } while (number <= 0);
+
+    return number;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], short arrLength)
+{
+    for (short i = 0; i < arrLength; i++)
+    {
+        arr[i] = GetRandomNumber(1, 100);
+    }
+}
+
+void PrintArray(int arr[100], short arrLength)
+{
+    for (short i = 0; i < arrLength; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int MinElementInArray(int arr[100], short arrLength)
+{
+    int min = arr[0];
+    for (short i = 0; i < arrLength; i++)
+    {
+        if (min > arr[i])
+            min = arr[i];
+    }
+    return min;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[100];
+    short arrLength = ReadPositiveNumber("Enter number of elements: ");
+
+    FillArrayWithRandomNumbers(arr, arrLength);
+    PrintArray(arr, arrLength);
+
+    cout << "\nMin Number is: " << MinElementInArray(arr, arrLength) << endl;
+
+    return 0;
+}
